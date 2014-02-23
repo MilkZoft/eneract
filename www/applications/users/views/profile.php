af3e2e62e7d8c40190c819162163a662aa8b6d70
Shawn Lee <shawnsmlee@gmail.com>
2014-02-23 13:38:00 -0800

    new form

--- www/applications/users/views/profile.php
+++ www/applications/users/views/profile.php
@@ -24,7 +24,73 @@ $serviceID  = recoverPOST("serviceID", $socialUser["serviceID"]);
 			<?php echo __("Phone"); ?>: 
 			<input class="form-control" placeholder="<?php echo __("XXX-XXX-XXXX"); ?>" name="phone" type="tel" />
 		</p>
-
+		<h4>Household Information</h4>
+		<div class="form-group">
+    		<label for="houseNum">Number in household</label>
+    		<input type="number" class="form-control col-xs-3" id="houseNum" placeholder="Enter a number">
+ 		</div>
+ 		<div class="form-group">
+    		<label for="houseSize">Size of the house (in sq. ft)</label>
+    		<input type="number" class="form-control col-xs-3" id="houseSize" placeholder="Enter a number">
+ 		</div>
+ 		<select class="form-control">
+			<option>United States</option>
+			<option>Canada</option>
+			<option>Mexico</option>
+			<option>Spain</option>
+		</select>
+ 		<div class="form-group">
+    		<label for="zipCode">ZipCode</label>
+    		<input type="number" class="form-control col-xs-3" id="zipCode" placeholder="Zip Code">
+ 		</div>
+ 		<br>
+		<h4>Car 1</h4>
+		<div class="radio">
+			<label>
+				<input type="radio" name="car1radios" checked> Gas
+			</label>
+		</div>
+		<div class="radio">
+			<label>
+				<input type="radio" name="car1radios" checked> Hybrid
+			</label>
+		</div>
+		<div class="radio">
+			<label>
+				<input type="radio" name="car1radios" checked> Electric
+			</label>
+		</div>
+		<br>
+		<h4>Car 2</h4>
+		<div class="radio">
+			<label>
+				<input type="radio" name="car2radios" checked> Gas
+			</label>
+		</div>
+		<div class="radio">
+			<label>
+				<input type="radio" name="car2radios" checked> Hybrid
+			</label>
+		</div>
+		<div class="radio">
+			<label>
+				<input type="radio" name="car2radios" checked> Electric
+			</label>
+		</div>
+		<br>
+		<h4>Transportation to Work</h4>
+		<label>
+      		<input type="checkbox"> Car
+      	</label>
+      	<label>
+			<input type="checkbox"> Carpool
+		</label>
+      	<label>
+			<input type="checkbox"> Bus
+		</label>
+      	<label>
+			<input type="checkbox"> Bike
+		</label>
 		<p>
 			<input  class="btn btn-lg btn-primary btn-block" name="complete" type="submit" class="btn" value="<?php echo __("Complete Profile"); ?>" />
 		</p>
